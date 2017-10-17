49fadc31488878239ec483c5ec128b87a4cb1cc3
Cedric Laier <Rintel@users.noreply.github.com>
2017-10-17 20:34:01 +0200

    added needed babelrc file

--- src/app.js
+++ src/app.js
@@ -1,6 +1,4 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import MyApp from "./components/MyApp";
-import "normalize.css/normalize.css";
 
-ReactDOM.render(<MyApp />, document.getElementById("app"));
+ReactDOM.render(<div>Test</div>, document.getElementById("app"));
