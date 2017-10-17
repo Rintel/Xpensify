e1b66b62114addd3caa6cb653df6bdb0431af983
Cedric Laier <Rintel@users.noreply.github.com>
2017-10-17 21:54:21 +0200

    paying around with the menu drawer

--- src/components/MaterialButtonTest.js
+++ src/components/MaterialButtonTest.js
@@ -1,8 +1,29 @@
 import React from 'react';
 import RaisedButton from 'material-ui/RaisedButton';
+import Drawer from 'material-ui/Drawer';
+import MenuItem from 'material-ui/MenuItem';
 
-const MaterialButtonTest = () => (
-  <RaisedButton label="Default" />
-);
+export default class MaterialButtonTest extends React.Component {
 
-export default MaterialButtonTest;
+  constructor(props) {
+    super(props);
+    this.state = {open: false};
+  }
+
+  handleToggle = () => this.setState({open: !this.state.open});
+
+  render() {
+    return (
+      <div>
+        <Drawer open={this.state.open}>
+          <MenuItem onClick={this.handleToggle}>Close</MenuItem>
+          <MenuItem>Menu Item 2</MenuItem>
+        </Drawer>
+        <RaisedButton
+          label="Toggle Drawer"
+          onClick={this.handleToggle}
+        />
+      </div>
+    );
+  }
+}
