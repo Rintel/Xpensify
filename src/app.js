e6645dd9689e6e145ea31fe190a97fcf0c3f90c1
Cedric Laier <Rintel@users.noreply.github.com>
2017-10-17 12:05:18 +0200

    setup project

---
+++ src/app.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import MyApp from "./components/MyApp";
+import "normalize.css/normalize.css";
+
+ReactDOM.render(<MyApp />, document.getElementById("app"));
