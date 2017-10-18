23a18d2d69e29658ea2063a72a9e0063362f7a76
Rintel <ced.laier@gmail.com>
2017-10-18 18:06:56 +0200

    add padding to list and header

--- src/components/MaterialButtonTest.js
+++ src/components/MaterialButtonTest.js
@@ -4,12 +4,13 @@ import Drawer from 'material-ui/Drawer';
 import MenuItem from 'material-ui/MenuItem';
 import List from './List/List';
 import Header from './Header/Header';
+import './style.css';
 
 export default class MaterialButtonTest extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = {open: false};
+    this.state = {open: true};
   }
 
   handleToggle = () => this.setState({open: !this.state.open});
@@ -17,15 +18,18 @@ export default class MaterialButtonTest extends React.Component {
   render() {
     return (
       <div>
-        <Header
-          title={"Xpensify"}
-          togglemenu={this.handleToggle}
-        />
+        <div className="content-container">
+          <Header
+            title={"Xpensify"}
+            togglemenu={this.handleToggle}
+          />
+          <List />
+        </div>
         <Drawer open={this.state.open}>
           <MenuItem onClick={this.handleToggle}>Close</MenuItem>
           <MenuItem>Menu Item 2</MenuItem>
         </Drawer>
-        <List />
+
       </div>
     );
   }
